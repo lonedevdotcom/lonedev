a949ba6121a4327f42b492520cf84826b30af3d9
richard_hawkes@yahoo.co.uk <richard_hawkes@yahoo.co.uk@ef9b63f8-4398-21e3-ea95-7f6a244bea72>
2011-01-09 19:18:49 +0000

    Import into SVN for multi desktop/laptop updates!

---
+++ RocketServer/src/com/lonedev/gtroot/server/ClientMessageType.java
@@ -0,0 +1,5 @@
+package com.lonedev.gtroot.server;
+
+public class ClientMessageType {
+    public static final int JOIN_TABLE = 1;
+}
---
+++ RocketServer/src/com/lonedev/gtroot/server/PlayerPosition.java
@@ -0,0 +1,9 @@
+package com.lonedev.gtroot.server;
+
+/**
+ *
+ * @author Richard
+ */
+public enum PlayerPosition {
+    PLAYER1, PLAYER2, PLAYER3, PLAYER4;
+}
