f9e486b3416a3bf38ec99350d74c721dcd5f9ab2
richard_hawkes@yahoo.co.uk <richard_hawkes@yahoo.co.uk@ef9b63f8-4398-21e3-ea95-7f6a244bea72>
2011-01-10 20:29:01 +0000

    More of the same

---
+++ trunk/RocketShared/src/com/lonedev/gtroot/shared/DiceFaces.java
@@ -0,0 +1,10 @@
+package com.lonedev.gtroot.shared;
+
+/**
+ * Intended to show the various respective colour values of the dice numbers.
+ * 
+ * @author Richard Hawkes
+ */
+public class DiceFaces {
+
+}
