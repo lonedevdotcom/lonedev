66aa9d80ac1a0c73f55d1821b757ece3d4292dd3
richard_hawkes <richard_hawkes@yahoo.co.uk>
2011-01-11 21:26:23 +0000

    Refactor naming

---
+++ RocketShared/src/com/lonedev/gtroot/shared/ClientServerMessageInteractor.java
@@ -0,0 +1,19 @@
+package com.lonedev.gtroot.shared;
+
+import java.text.DecimalFormat;
+import java.text.Format;
+
+public class ClientServerMessageInteractor {
+    public static final int JOIN_TABLE = 1;
+    public static final int PLAYER_JOINED_TABLE = 2;
+    
+    private static Format numberFormat = new DecimalFormat("00000");
+
+    public static String createJoinTableRequestMessage() {
+        return numberFormat.format(JOIN_TABLE) + "ARGS";
+    }
+
+    public static String createTableJoinSuccessMessage(String playerName, int playerPosition) {
+        return numberFormat.format(PLAYER_JOINED_TABLE) + ":" + playerName + ":" + playerPosition;
+    }
+}
