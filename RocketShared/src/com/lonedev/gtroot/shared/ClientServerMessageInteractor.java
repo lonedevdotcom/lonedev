aa36efb7600d83e295ea052a069e86c7570a1b93
richard_hawkes <richard_hawkes@yahoo.co.uk>
2011-01-18 13:46:27 +0000

    More of the same

--- RocketShared/src/com/lonedev/gtroot/shared/ClientServerMessageInteractor.java
+++ RocketShared/src/com/lonedev/gtroot/shared/ClientServerMessageInteractor.java
@@ -6,6 +6,7 @@ import java.text.Format;
 public class ClientServerMessageInteractor {
     public static final int JOIN_TABLE = 1;
     public static final int PLAYER_JOINED_TABLE = 2;
+    public static final int TABLE_STATUS_REQUEST = 3;
     
     private static Format numberFormat = new DecimalFormat("00000");
 
