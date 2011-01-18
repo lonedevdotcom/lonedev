5e66e6cb836358533fb679ae0258a80b73480366
richard_hawkes@yahoo.co.uk <richard_hawkes@yahoo.co.uk@ef9b63f8-4398-21e3-ea95-7f6a244bea72>
2011-01-18 13:05:58 +0000

    More of the same

---
+++ trunk/RocketShared/src/com/lonedev/gtroot/shared/RocketServerConstants.java
@@ -0,0 +1,9 @@
+package com.lonedev.gtroot.shared;
+
+/**
+ *
+ * @author Richard Hawkes
+ */
+public class RocketServerConstants {
+    public static final String SERVER_CHAT_CHANNEL = "ServerChatChannel";
+}
