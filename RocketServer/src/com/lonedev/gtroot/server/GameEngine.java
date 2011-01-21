4682500d807ec82c054e9630f68117f7895003ae
richard_hawkes <richard_hawkes@yahoo.co.uk>
2011-01-21 18:29:26 +0000

    More of the same

---
+++ RocketServer/src/com/lonedev/gtroot/server/GameEngine.java
@@ -0,0 +1,36 @@
+package com.lonedev.gtroot.server;
+
+/**
+ * Implementations of this interface handle a table current game status (ie
+ * scores). The table will pass requests to it, and it should update the
+ * game status as appropriate. A call to getGameStatus() will return a string
+ * that is passed straight back on the tables channel.
+ *
+ * I am pretty sure any implementation must implement Serializable too.
+ *
+ * @author Richard Hawkes
+ */
+public interface GameEngine {
+    /**
+     * Handles the roll request from a given player. Probably just rolls the
+     * dice.
+     *
+     * @param player The player making the request (may not be relevant)...
+     */
+    public void processRollRequest(RocketPlayer player);
+
+    /**
+     * This method handles the players decisions based on the roll they got.
+     *
+     * @param player The player making the request (may not be relevant)...
+     * @param response The response (ie decision) that they made. This would be
+     * used to update the games status.
+     */
+    public void processRollResponse(RocketPlayer player, String response);
+
+    /**
+     * Returns a string with the current game status (ie scores). This is passed
+     * directly back on the tables channel.
+     */
+    public String getGameStatus();
+}
