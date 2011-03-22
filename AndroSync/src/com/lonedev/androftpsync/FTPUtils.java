eab8ae3661b4c4cc1bfc1e64d30a81ff93c4761a
richard_hawkes <richard_hawkes@yahoo.co.uk>
2011-03-22 06:54:54 +0000

    New additions

---
+++ AndroSync/src/com/lonedev/androftpsync/FTPUtils.java
@@ -0,0 +1,75 @@
+package com.lonedev.androftpsync;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPReply;
+
+import android.util.Log;
+
+public class FTPUtils {
+	private String ftpUsername, ftpHostname, ftpPassword;
+	private int ftpPort;
+	private FTPClient ftpClient;
+	
+	public static final String TAG = "FTPUtils";
+	public static final int DEFAULT_FTP_PORT = 21;
+	
+	public FTPUtils (String ftpHostname, int ftpPort, String ftpUsername, String ftpPassword) throws SocketException, IOException {
+		this.ftpUsername = ftpUsername;
+		this.ftpHostname = ftpHostname;
+		this.ftpPort = ftpPort;
+		this.ftpPassword = ftpPassword;
+//		connect();
+	}
+
+	public FTPUtils (String ftpHostname, String ftpUsername, String ftpPassword) throws SocketException, IOException {
+		this(ftpHostname, DEFAULT_FTP_PORT, ftpUsername, ftpPassword);
+	}
+	
+	public void connect() throws SocketException, IOException {
+		Log.i(TAG, "Test attempt login to " + ftpHostname + " as " + ftpUsername);
+
+		ftpClient = new FTPClient();
+		
+		ftpClient.connect(this.ftpHostname, this.ftpPort);
+		ftpClient.login(ftpUsername, ftpPassword);
+
+		int reply = ftpClient.getReplyCode();
+
+		if (!FTPReply.isPositiveCompletion(reply)) {
+			String error = "Login failure (" + reply + ") : " + ftpClient.getReplyString();
+			Log.e(TAG, error);
+			throw new IOException(error);
+		} 
+	}
+	
+	public void syncToFTPDirectory(File localFolder, File remoteFolder) {
+		List<File> syncLocalFiles = new ArrayList<File>();
+		getFiles(localFolder, syncLocalFiles);
+	}
+	
+	private void getFiles(File folder, List<File> syncFiles) {
+		File[] folderFiles = folder.listFiles();
+		
+		for (File file : folderFiles) {
+			if (file.isDirectory()) {
+				getFiles(file, syncFiles);
+			} else if (file.isFile()) {
+				syncFiles.add(file);
+			}
+		}
+	}
+
+	public void disconnect() throws IOException {
+		if (ftpClient != null) {
+			Log.i(TAG, "Disconnecting from " + ftpHostname);
+			ftpClient.disconnect();
+		}
+	}
+	
+}
