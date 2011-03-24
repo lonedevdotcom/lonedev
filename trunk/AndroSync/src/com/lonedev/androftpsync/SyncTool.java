c9142ffae5c6a3ff9371f744d0860efd34ba8724
richard_hawkes@yahoo.co.uk <richard_hawkes@yahoo.co.uk@ef9b63f8-4398-21e3-ea95-7f6a244bea72>
2011-03-24 07:15:10 +0000

    New class

---
+++ trunk/AndroSync/src/com/lonedev/androftpsync/SyncTool.java
@@ -0,0 +1,13 @@
+package com.lonedev.androftpsync;
+
+/**
+ * This is the class that can do all of the necessary syncing to/from the
+ * remote server.
+ * 
+ * @author Richard Hawkes
+ */
+public class SyncTool {
+	public void sync() {
+		//TODO Sync it !!
+	}
+}
