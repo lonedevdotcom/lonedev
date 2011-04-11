f142c8c1894b36df2a94054d095219b94fbbb517
richard_hawkes@yahoo.co.uk <richard_hawkes@yahoo.co.uk@ef9b63f8-4398-21e3-ea95-7f6a244bea72>
2011-04-11 07:18:17 +0000

    Putting into correct location

---
+++ trunk/SQLiteJViewer/src/sqlitejviewer/DatabaseDataType.java
@@ -0,0 +1,5 @@
+package sqlitejviewer;
+
+public enum DatabaseDataType {
+    INTEGER, REAL, TEXT, NUMERIC, BLOB, NONE;
+}
---
+++ trunk/SQLiteJViewer/src/sqlitejviewer/DatabaseObjectDTO.java
@@ -0,0 +1,38 @@
+package sqlitejviewer;
+
+public abstract class DatabaseObjectDTO {
+    private String name, sql;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the sql
+     */
+    public String getSql() {
+        return sql;
+    }
+
+    /**
+     * @param sql the sql to set
+     */
+    public void setSql(String sql) {
+        this.sql = sql;
+    }
+
+    @Override
+    public String toString() {
+        return this.getName();
+    }
+}
