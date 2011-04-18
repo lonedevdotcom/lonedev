8a293b4c0a6bf2e15c76a4ebe22d5766d1d246eb
richard_hawkes <richard_hawkes@yahoo.co.uk>
2011-04-18 15:20:08 +0000

    Nothing special.

--- SQLiteJViewer/test/sqlitejviewer/SQLiteDatabaseGUIInteractorTest.java
+++ SQLiteJViewer/test/sqlitejviewer/SQLiteDatabaseGUIInteractorTest.java
@@ -3,9 +3,7 @@ package sqlitejviewer;
 import java.io.File;
 import javax.swing.table.TableModel;
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -13,14 +11,6 @@ import static org.junit.Assert.*;
 public class SQLiteDatabaseGUIInteractorTest {
 
     private SQLiteDatabaseGUIInteractor dbInteractor;
-
-//    @BeforeClass
-//    public static void setUpClass() throws Exception {
-//    }
-//
-//    @AfterClass
-//    public static void tearDownClass() throws Exception {
-//    }
 
     @Before
     public void setUp() {
